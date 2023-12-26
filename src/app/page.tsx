fc2fdb450d3968b3dd8e0aaa9ceddbebf9476b14
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2023-12-27 06:39:40 +0900

    Feat: 지도 표시(진행중)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Map from "@/components/map";
+import Script from "next/script";
+
 export default function Home() {
-  return <main>하이</main>;
+  return (
+    <main>
+      <Script
+        type="text/javascript"
+        src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.MAP_CLIENT_ID}`}
+      />
+      {/* <Map /> */}
+    </main>
+  );
 }
---
+++ src/components/map.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Map() {
+  return (
+    <div>
+      <div id="map" style={{ width: "100%", height: "400px" }}></div>
+    </div>
+  );
+}
